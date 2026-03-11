0669f8e91b9402438e88b82a30640c41906072a0
aleksapajic02 <apajic5921rn@raf.rs>
2026-03-11 14:39:30 +0100

    Commented lines in user service
    Uncomment to use notification service.

--- cmd/user/main.go
+++ cmd/user/main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"banka-raf/gen/notification"
 	"banka-raf/gen/user"
 	internalUser "banka-raf/internal/user"
-	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
-
+	//"context" for notifications
+	//"banka-raf/gen/notification"
+	//"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
 )
 
@@ -31,32 +30,24 @@ func main() {
 	reflection.Register(srv)
 
 	log.Printf("user service listening on :%s", port)
-	conn, err := grpc.Dial(
+	//stvaranje konekcije sa servisom
+	/*conn, err := grpc.Dial(
 		"notification:50051",
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	)
 	if err != nil {
 		log.Fatal(err)
-	}
-
-	client := notification.NewNotificationServiceClient(conn)
-
-	resp, err := client.SendActivationEmail(context.Background(), &notification.ActivationMailRequest{
-		ToAddr: "pajicaleksa.12@gmail.com",
-		Link:   "servis.raf.edu.rs",
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	resp2, err := client.SendConfirmationEmail(context.Background(), &notification.ConfirmationMailRequest{
-		ToAddr: "pajicaleksa.12@gmail.com",
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("resp2: %v", resp2)
-	log.Printf("resp: %s", resp)
+	}*/
+	//pravljenje klijenta
+	//client := notification.NewNotificationServiceClient(conn)
+	//poziv neke metode servisa
+	/*
+		resp, err := client.SendActivationEmail(context.Background(), &notification.ActivationMailRequest{
+			ToAddr: "pajicaleksa.12@gmail.com",
+			Link:   "servis.raf.edu.rs",
+		})
+		if err != nil {log.Fatal(err)}
+		log.Printf("resp: %s", resp)*/
 
 	if err := srv.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
