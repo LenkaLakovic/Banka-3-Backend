839f98f20d4b2e4d8914e7e316c2d1937b87212d
Aleksa Janic <aleksajanic.dev@gmail.com>
2026-03-14 15:32:50 +0100

    Add gateway handlers for account creation

--- internal/gateway/handlers.go
+++ internal/gateway/handlers.go
@@ -15,22 +15,26 @@ func SetupApi(router *gin.Engine, server *Server) {
 	router.GET("/healthz", server.Healthz)
 
 	api := router.Group("/api")
-
 	{
 		api.POST("/login", server.Login)
 		api.POST("/logout", AuthenticatedMiddleware(server.UserClient), server.Logout)
 		api.POST("/token/refresh", server.Refresh)
 	}
 
-	password_reset := api.Group("/password-reset")
+	passwordReset := api.Group("/password-reset")
 	{
-		password_reset.POST("/request", server.RequestPasswordReset)
-		password_reset.POST("/confirm", server.ConfirmPasswordReset)
+		passwordReset.POST("/request", server.RequestPasswordReset)
+		passwordReset.POST("/confirm", server.ConfirmPasswordReset)
+	}
+
+	clients := api.Group("/clients")
+	{
+		clients.POST("", server.CreateClientAccount)
 	}
 
 	employees := api.Group("/employees")
-
 	{
+		employees.POST("", server.CreateEmployeeAccount)
 		employees.GET("/:id", server.GetEmployeeByID)
 	}
 }
@@ -45,10 +49,10 @@ func (s *Server) Logout(c *gin.Context) {
 
 	ctx, cancel := context.WithTimeout(c.Request.Context(), 5*time.Second)
 	defer cancel()
+
 	_, err := s.UserClient.Logout(ctx, &userpb.LogoutRequest{
 		Email: email,
 	})
-
 	if err != nil {
 		writeGRPCError(c, err)
 		return
@@ -106,6 +110,83 @@ func (s *Server) Refresh(c *gin.Context) {
 	})
 }
 
+func (s *Server) CreateClientAccount(c *gin.Context) {
+	var req createClientAccountRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.String(http.StatusBadRequest, "invalid json body")
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(c.Request.Context(), 5*time.Second)
+	defer cancel()
+
+	resp, err := s.UserClient.CreateClientAccount(ctx, &userpb.CreateClientRequest{
+		FirstName:   req.FirstName,
+		LastName:    req.LastName,
+		DateOfBirth: req.DateOfBirth,
+		Gender:      req.Gender,
+		Email:       req.Email,
+		PhoneNumber: req.PhoneNumber,
+		Address:     req.Address,
+		Password:    req.Password,
+	})
+	if err != nil {
+		writeGRPCError(c, err)
+		return
+	}
+
+	if resp.Valid {
+		c.JSON(http.StatusCreated, gin.H{
+			"valid": true,
+		})
+		return
+	}
+
+	c.JSON(http.StatusUnprocessableEntity, gin.H{
+		"valid": false,
+	})
+}
+
+func (s *Server) CreateEmployeeAccount(c *gin.Context) {
+	var req createEmployeeAccountRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.String(http.StatusBadRequest, "invalid json body")
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(c.Request.Context(), 5*time.Second)
+	defer cancel()
+
+	resp, err := s.UserClient.CreateEmployeeAccount(ctx, &userpb.CreateEmployeeRequest{
+		FirstName:   req.FirstName,
+		LastName:    req.LastName,
+		DateOfBirth: req.DateOfBirth,
+		Gender:      req.Gender,
+		Email:       req.Email,
+		PhoneNumber: req.PhoneNumber,
+		Address:     req.Address,
+		Username:    req.Username,
+		Position:    req.Position,
+		Department:  req.Department,
+		Password:    req.Password,
+	})
+	if err != nil {
+		writeGRPCError(c, err)
+		return
+	}
+
+	if resp.Valid {
+		c.JSON(http.StatusCreated, gin.H{
+			"valid": true,
+		})
+		return
+	}
+
+	c.JSON(http.StatusUnprocessableEntity, gin.H{
+		"valid": false,
+	})
+}
+
 func (s *Server) GetEmployeeByID(c *gin.Context) {
 	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
 	if err != nil {
@@ -152,7 +233,6 @@ func (s *Server) RequestPasswordReset(c *gin.Context) {
 		return
 	}
 
-	// frontend should not be aware if the email got sent
 	c.JSON(http.StatusOK, gin.H{
 		"message": "If that email exists, a reset link was sent.",
 	})
@@ -176,10 +256,10 @@ func (s *Server) ConfirmPasswordReset(c *gin.Context) {
 		writeGRPCError(c, err)
 		return
 	}
+
 	if resp.Successful {
 		c.Status(http.StatusOK)
 	} else {
 		c.Status(http.StatusUnprocessableEntity)
 	}
-
 }
--- internal/gateway/types.go
+++ internal/gateway/types.go
@@ -17,3 +17,28 @@ type passwordResetConfirmationRequest struct {
 	Token       string `json:"token"`
 	NewPassword string `json:"password"`
 }
+
+type createClientAccountRequest struct {
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	DateOfBirth int64  `json:"date_of_birth"`
+	Gender      string `json:"gender"`
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phone_number"`
+	Address     string `json:"address"`
+	Password    string `json:"password"`
+}
+
+type createEmployeeAccountRequest struct {
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	DateOfBirth int64  `json:"date_of_birth"`
+	Gender      string `json:"gender"`
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phone_number"`
+	Address     string `json:"address"`
+	Username    string `json:"username"`
+	Position    string `json:"position"`
+	Department  string `json:"department"`
+	Password    string `json:"password"`
+}
